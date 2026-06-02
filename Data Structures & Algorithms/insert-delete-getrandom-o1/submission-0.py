ee03869b5d48ecd3c83edda42f8266f7343cf46a
Sanjay Deoram <47582804+sanjay-deoram@users.noreply.github.com>
2026-06-01 20:03:29 -0400

    Add: insert-delete-getrandom-o1 - submission-0

---
+++ Data Structures & Algorithms/insert-delete-getrandom-o1/submission-0.py
@@ -0,0 +1,30 @@
+class RandomizedSet:
+
+    def __init__(self):
+        self.rand_set = set()
+
+    def insert(self, val: int) -> bool:
+        if val not in self.rand_set:
+            self.rand_set.add(val)
+            return True
+        return False
+        
+    def remove(self, val: int) -> bool:
+        if val in self.rand_set:
+            self.rand_set.remove(val)
+            return True
+        return False
+
+    def getRandom(self) -> int:
+        # 1. approach is just to get the len and then randomize it like that
+        # 2. Keep a running tally in our class so we dont have to do len
+
+        return list(self.rand_set)[0]
+        
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
