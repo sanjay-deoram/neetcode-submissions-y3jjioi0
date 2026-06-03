98c9f5fbbd3b04c16800c6883d12308b4e2ddab2
Sanjay Deoram <47582804+sanjay-deoram@users.noreply.github.com>
2026-06-03 08:28:45 -0400

    Add: design-hit-counter - submission-3

---
+++ Data Structures & Algorithms/design-hit-counter/submission-3.py
@@ -0,0 +1,22 @@
+class HitCounter:
+
+    def __init__(self):
+        self.q = deque()
+
+    def hit(self, timestamp: int) -> None:
+        self.q.append(timestamp)
+
+    def getHits(self, timestamp: int) -> int:
+        while self.q:
+            diff = timestamp - self.q[0]
+            if diff >=300:
+                self.q.popleft()
+            else:
+                break
+        return len(self.q)
+
+
+# Your HitCounter object will be instantiated and called as such:
+# obj = HitCounter()
+# obj.hit(timestamp)
+# param_2 = obj.getHits(timestamp)
