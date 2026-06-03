fcc621e7eb406484181473e0990fb81f096874d0
Sanjay Deoram <47582804+sanjay-deoram@users.noreply.github.com>
2026-06-02 21:53:42 -0400

    Add: design-hit-counter - submission-1

---
+++ Data Structures & Algorithms/design-hit-counter/submission-1.py
@@ -0,0 +1,24 @@
+class HitCounter:
+    """
+    Brute Force solution
+    """
+    def __init__(self):
+        self.hits = deque()
+
+    def hit(self, timestamp: int) -> None:
+        self.hits.append(timestamp)
+
+    def getHits(self, timestamp: int) -> int:
+        while self.hits:
+            diff = timestamp - self.hits[0]
+            if diff >=300:
+                self.hits.popleft()
+            else:
+                break
+        return len(self.hits)
+
+
+# Your HitCounter object will be instantiated and called as such:
+# obj = HitCounter()
+# obj.hit(timestamp)
+# param_2 = obj.getHits(timestamp)
