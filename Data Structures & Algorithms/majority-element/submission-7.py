fa8ab3f968c41c0012543678249b43b3e67997b6
Sanjay Deoram <47582804+sanjay-deoram@users.noreply.github.com>
2026-06-08 19:33:14 -0400

    Add: majority-element - submission-7

---
+++ Data Structures & Algorithms/majority-element/submission-7.py
@@ -0,0 +1,18 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # boyer algo
+        count, res = 0, 0
+        prev = nums[0]
+
+        for i in range(len(nums)):
+            if count == 0:
+                res = nums[i]
+                count += 1
+            if nums[i] == prev:
+                count += 1
+                res = nums[i]
+            elif nums[i]!=prev:
+                count-=1
+            
+            print(f"Count:{count} | res: {res} | i:{i} | nums[i]:{nums[i]}")
+        return res
